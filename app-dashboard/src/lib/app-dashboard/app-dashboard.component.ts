6826dfd7cdf232fe97bad7504e27841588095059
Alejandro Garcia Vera <alejandrogarciav96@gmail.com>
2023-04-10 18:14:27 +0200

    hotfix

--- app-dashboard/src/lib/app-dashboard/app-dashboard.component.ts
+++ app-dashboard/src/lib/app-dashboard/app-dashboard.component.ts
@@ -1,50 +1,11 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import {
-    MultitabComponent,
-    PrimaryButtonComponent,
-    SecondaryButtonComponent,
-    TextboxComponent,
-} from "@web-app/shared/elements";
-import {
-    FormControl,
-    FormGroup,
-    ReactiveFormsModule,
-    Validators,
-} from "@angular/forms";
 
 @Component({
     standalone: true,
-    imports: [
-        CommonModule,
-        PrimaryButtonComponent,
-        SecondaryButtonComponent,
-        TextboxComponent,
-        ReactiveFormsModule,
-        MultitabComponent,
-    ],
+    imports: [CommonModule],
     templateUrl: "./app-dashboard.component.html",
     styleUrls: ["./app-dashboard.component.scss"],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppDashboardComponent {
-    isDisabled = false;
-    testForm = new FormGroup({
-        name: new FormControl("asdaaa", [
-            Validators.required,
-            Validators.maxLength(5),
-        ]),
-        name2: new FormControl("asdaaa", Validators.maxLength(5)),
-    });
-
-    selectedTab = 1;
-
-    toggleDisable() {
-        this.isDisabled = !this.isDisabled;
-        if (this.isDisabled) {
-            this.testForm.controls.name.disable();
-        } else {
-            this.testForm.controls.name.enable();
-        }
-    }
-}
+export class AppDashboardComponent {}
